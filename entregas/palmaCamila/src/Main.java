67555fd60f21c4101747e104565d9fc6e1b28968
Camila Lesly Palma <90413743+Camila-Lesly@users.noreply.github.com>
2026-05-11 09:39:56 +0000

    estudio

--- entregas/palmaCamila/src/Main.java
+++
@@ -1,8 +0,0 @@
-class Main {
-    public static void main(String[] args) {
-        TicTacToe ticTacToe = new TicTacToeBuilder()
-            .withView(new ConsoleView())
-            .build();
-        ticTacToe.play();
-    }
-}
--- entregas/palmaCamila/src/View.java
+++
@@ -1,6 +0,0 @@
-public interface View {
-    void displayBoard(char[][] cells);
-    Coordinate requestCoordinate();
-    void displayWinner(char color);
-    void displayMessage(String message);
-}
